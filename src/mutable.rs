af557e548bba8344bbaa1c19e460a505084f947a
jianghao <jianghao@haplox.com>
2024-01-17 14:17:20 +0800

    fix: 借用可变量和引用

--- src/mutable.rs
+++ src/mutable.rs
@@ -53,91 +53,93 @@ pub fn run_book() {
     // 改正 ^ 注释掉此行
 }
 
-// #[derive(Debug)]
-// struct Point {
-//     x: i32,
-//     y: i32,
-//     z: i32,
-// }
-
-fn change(some_string: &mut String) {
-    some_string.push_str("string");
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+    z: i32,
 }
 
+// fn change(some_string: &mut String) {
+//     some_string.push_str("string");
+// }
+
 pub fn run_point() {
-    let mut x = 6;
-    let y = &mut x;
-    *y = *y + 1;
-    println!("{}", y);
-    let mut s = String::from("hello");
-    change(&mut s);
-    s.push_str("55555");
-    println!("{:?}", s);
-    // let mut point = Point { x: 0, y: 0, z: 0 };
+    // let mut x = 6;
+    // let y = &mut x;
+    // *y = *y + 1;
+    // println!("{}", y);
+    // let mut s = String::from("hello");
+    // change(&mut s);
+    // s.push_str("55555");
+    // println!("{:?}", s);
+    let mut point = Point { x: 0, y: 0, z: 0 };
 
-    // let borrowed_point = &point;
-    // let another_borrow = &point;
+    let borrowed_point = &point;
+    let another_borrow = &point;
 
-    // // 数据可以通过引用或原始类型来访问
-    // println!(
-    //     "Point has coordinates: ({}, {}, {})",
-    //     borrowed_point.x, another_borrow.y, point.z
-    // );
-
-    // // // 报错！`point` 不能以可变方式借用，因为当前还有不可变借用。
-    // // let mutable_borrow = &mut point;
-    // // // TODO ^ 试一试去掉此行注释
-    // // println!("{:?}", mutable_borrow);
-
-    // // 被借用的值在这里被重新使用
-    // println!(
-    //     "Point has coordinates: ({}, {}, {})",
-    //     borrowed_point.x, another_borrow.y, point.z
-    // );
-    // // 数据可以通过引用或原始类型来访问
-    // println!(
-    //     "Point has coordinates: ({}, {}, {})",
-    //     borrowed_point.x, another_borrow.y, point.z
-    // );
+    // 数据可以通过引用或原始类型来访问
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        borrowed_point.x, another_borrow.y, point.z
+    );
 
     // // 报错！`point` 不能以可变方式借用，因为当前还有不可变借用。
     // let mutable_borrow = &mut point;
-    // TODO ^ 试一试去掉此行注释
+    // // TODO ^ 试一试去掉此行注释
+    // println!("{:?}", mutable_borrow);
 
     // 被借用的值在这里被重新使用
     // println!(
     //     "Point has coordinates: ({}, {}, {})",
     //     borrowed_point.x, another_borrow.y, point.z
     // );
-    // println!("{:?}", point);
-    // println!("{}", mutable_borrow.z);
-
-    // // 不可变的引用不再用于其余的代码，因此可以使用可变的引用重新借用。
-    // let mutable_borrow = &mut point;
-
-    // // 通过可变引用来修改数据
-    // mutable_borrow.x = 5;
-    // mutable_borrow.y = 2;
-    // mutable_borrow.z = 1;
-
-    // // 报错！不能再以不可变方式来借用 `point`，因为它当前已经被可变借用。
-    // // let y = &point.y;
-    // // TODO ^ 试一试去掉此行注释
-
-    // // 报错！无法打印，因为 `println!` 用到了一个不可变引用。
-    // // println!("Point Z coordinate is {}", point.z);
-    // // TODO ^ 试一试去掉此行注释
-
-    // // 正常运行！可变引用能够以不可变类型传入 `println!`
+    // 数据可以通过引用或原始类型来访问
     // println!(
     //     "Point has coordinates: ({}, {}, {})",
-    //     mutable_borrow.x, mutable_borrow.y, mutable_borrow.z
+    //     borrowed_point.x, another_borrow.y, point.z
     // );
 
-    // // 可变引用不再用于其余的代码，因此可以重新借用
-    // let new_borrowed_point = &point;
+    // 报错！`point` 不能以可变方式借用，因为当前还有不可变借用。
+    // let mutable_borrow = &mut point;
+    // TODO ^ 试一试去掉此行注释
+
+    // 被借用的值在这里被重新使用
     // println!(
-    //     "Point now has coordinates: ({}, {}, {})",
-    //     new_borrowed_point.x, new_borrowed_point.y, new_borrowed_point.z
+    //     "Point has coordinates: ({}, {}, {})",
+    //     borrowed_point.x, another_borrow.y, mutable_borrow.z
     // );
+    // println!("{}", mutable_borrow.z);
+
+    // 不可变的引用不再用于其余的代码，因此可以使用可变的引用重新借用。
+    let mutable_borrow = &mut point;
+
+    // 通过可变引用来修改数据
+    mutable_borrow.x = 5;
+    mutable_borrow.y = 2;
+    mutable_borrow.z = 1;
+
+    // 报错！不能再以不可变方式来借用 `point`，因为它当前已经被可变借用。
+    // let y = &point.y;
+    // TODO ^ 试一试去掉此行注释
+
+    // 报错！无法打印，因为 `println!` 用到了一个不可变引用。
+    // println!("Point Z coordinate is {}", point.z);
+    // TODO ^ 试一试去掉此行注释
+
+    // 正常运行！可变引用能够以不可变类型传入 `println!`
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        mutable_borrow.x, mutable_borrow.y, mutable_borrow.z
+    );
+
+    // 可变引用不再用于其余的代码，因此可以重新借用
+    let new_borrowed_point = &mut point;
+    println!(
+        "Point now has coordinates: ({}, {}, {})",
+        new_borrowed_point.x, new_borrowed_point.y, new_borrowed_point.z
+    );
+
+    // 错误 point 可变量 已被new_borrowed_point 借用
+    // println!("point.z is {}", mutable_borrow.z);
 }
